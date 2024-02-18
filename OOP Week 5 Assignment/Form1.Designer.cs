e470052ffeb4c0c3e2b90499c5e005bfeafec679
Oliver Crawford <oicrawford@gmail.com>
2024-02-17 23:37:07 -0600

    Add project files.

---
+++ OOP Week 5 Assignment/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace OOP_Week_5_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItemNew = new System.Windows.Forms.MenuItem();
+            this.menuItemNewWindow = new System.Windows.Forms.MenuItem();
+            this.menuItemOpen = new System.Windows.Forms.MenuItem();
+            this.menuItemSave = new System.Windows.Forms.MenuItem();
+            this.menuItemSaveAs = new System.Windows.Forms.MenuItem();
+            this.menuItem7 = new System.Windows.Forms.MenuItem();
+            this.rTxtBMain = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1});
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.Index = 0;
+            this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItemNew,
+            this.menuItemNewWindow,
+            this.menuItemOpen,
+            this.menuItemSave,
+            this.menuItemSaveAs,
+            this.menuItem7});
+            this.menuItem1.Text = "File";
+            // 
+            // menuItemNew
+            // 
+            this.menuItemNew.Index = 0;
+            this.menuItemNew.Text = "New";
+            this.menuItemNew.Click += new System.EventHandler(this.menuItemNew_Click);
+            // 
+            // menuItemNewWindow
+            // 
+            this.menuItemNewWindow.Index = 1;
+            this.menuItemNewWindow.Text = "New Window";
+            this.menuItemNewWindow.Click += new System.EventHandler(this.menuItemNewWindow_Click);
+            // 
+            // menuItemOpen
+            // 
+            this.menuItemOpen.Index = 2;
+            this.menuItemOpen.Text = "Open";
+            this.menuItemOpen.Click += new System.EventHandler(this.menuItemOpen_Click);
+            // 
+            // menuItemSave
+            // 
+            this.menuItemSave.Index = 3;
+            this.menuItemSave.Text = "Save";
+            this.menuItemSave.Click += new System.EventHandler(this.menuItemSave_Click);
+            // 
+            // menuItemSaveAs
+            // 
+            this.menuItemSaveAs.Index = 4;
+            this.menuItemSaveAs.Text = "Save As";
+            this.menuItemSaveAs.Click += new System.EventHandler(this.menuItemSaveAs_Click);
+            // 
+            // menuItem7
+            // 
+            this.menuItem7.Index = 5;
+            this.menuItem7.Text = "Exit";
+            this.menuItem7.Click += new System.EventHandler(this.menuItem7_Click);
+            // 
+            // rTxtBMain
+            // 
+            this.rTxtBMain.Location = new System.Drawing.Point(1, -1);
+            this.rTxtBMain.Name = "rTxtBMain";
+            this.rTxtBMain.Size = new System.Drawing.Size(799, 450);
+            this.rTxtBMain.TabIndex = 0;
+            this.rTxtBMain.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rTxtBMain);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItemNew;
+        private System.Windows.Forms.MenuItem menuItemNewWindow;
+        private System.Windows.Forms.MenuItem menuItemOpen;
+        private System.Windows.Forms.MenuItem menuItemSave;
+        private System.Windows.Forms.MenuItem menuItemSaveAs;
+        private System.Windows.Forms.MenuItem menuItem7;
+        private System.Windows.Forms.RichTextBox rTxtBMain;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ OOP Week 5 Assignment/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace OOP_Week_5_Assignment
+{
+    public partial class Form1 : Form
+    {
+        //this is to store the last used file path, when saving the file it will automatically use this
+        string filePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "New File";
+        }
+
+        private void menuItem7_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void menuItemNew_Click(object sender, EventArgs e)
+        {
+            rTxtBMain.Text = "";
+            filePath = "";
+            this.Text = "New File";
+        }
+
+        private void menuItemNewWindow_Click(object sender, EventArgs e)
+        {
+            Form1 form = new Form1();
+            form.filePath = "";
+            form.Show();
+        }
+
+        private void menuItemOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                this.Text = filePath;
+                using (StreamReader sr = new StreamReader(openFileDialog1.FileName))
+                {
+                    rTxtBMain.Text = "";
+                    rTxtBMain.Text += sr.ReadToEnd();
+                }
+            }
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void menuItemSave_Click(object sender, EventArgs e)
+        {
+            if (filePath == "")
+            {
+                menuItemSaveAs_Click(null, null);
+            }else
+            {
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.Write(rTxtBMain.Text);
+                }
+            }
+        }
+
+        private void menuItemSaveAs_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                InitialDirectory = @"",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true
+            };
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filePath = saveFileDialog1.FileName;
+                this.Text = filePath;
+                using (StreamWriter sw = new StreamWriter(filePath))
+                {
+                    sw.Write(rTxtBMain.Text);
+                }
+            }
+        }
+    }
+}
